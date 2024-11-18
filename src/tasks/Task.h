8aa15d0fc7afe38f56c10fb4cee1af854fe76666
Michele Farneti <michele.farneti@studio.unibo.it>
2024-11-19 00:14:32 +0100

    Tried implemementation of a basic schedluer layout

---
+++ src/tasks/Task.h
@@ -0,0 +1,32 @@
+#ifndef __TASK__
+#define __TASK__
+
+/**
+ * Abastract class rappresenting an interface for a generica task of FSM.
+ */
+class Task {
+    int myPeriod;
+    int timeElapsed;
+
+    public:
+    virtual void init(int period){
+        myPeriod = period;  
+        timeElapsed = 0;
+    }
+
+    /* Handler for the tick of the FSM*/
+    virtual void tick() = 0;
+
+    bool updateAndCheckTime(int basePeriod){
+        timeElapsed += basePeriod;
+        if (timeElapsed >= myPeriod){
+            timeElapsed = 0;
+            return true;
+        } else {
+            return false; 
+        }
+    }
+  
+};
+
+#endif
