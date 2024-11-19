25d4f706bf4e375b71fb02af0a03893f4b1305b1
Michele farneti <michele.farneti@studio.unibo.it>
2024-11-19 13:03:03 +0100

    Implemented base layout for dashboard

---
+++ src/dashboard/Dashboard.cpp
@@ -0,0 +1,13 @@
+#include "dashboard/Dashboard.h"
+
+Dashboard::Dashboard(){
+
+}
+
+void Dashboard::communicateTemperature(){
+    
+}
+
+void Dashboard::communicateLevel(){
+    
+}
---
+++ src/dashboard/Dashboard.h
@@ -0,0 +1,11 @@
+#ifndef __DASHBOARD__
+#define __DASHBOARD__
+
+class Dashboard{
+    public:
+    Dashboard();
+    void communicateTemperature();
+    void communicateLevel();
+};
+
+#endif
