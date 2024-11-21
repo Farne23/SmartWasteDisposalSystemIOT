80a90c8fdeba8dbb3fcf45c7978892a154df8271
Michele Farneti <michele.farneti@studio.unibo.it>
2024-11-21 20:54:48 +0100

    Added light signals management to  the container implementation

--- Arduino/src/container/Container.cpp
+++ Arduino/src/container/Container.cpp
@@ -3,8 +3,15 @@
 
 Container::Container(){
     this->tempSensor = new TempSensor(TEMP_SENSOR_PIN);
+    this->lightSignals = new LightSignals(new Led(GREEN_LED_PIN), new Led(RED_LED_PIN));
+    this->status=NORMAL;
 }
 
 double Container::readTemperature(){
     return this->tempSensor->sense();
+}
+
+void Container::signalProblem(){
+    this->status=PROBLEM_DETECTED;
+    this->lightSignals->signalProblem();
 }
--- Arduino/src/container/Container.h
+++ Arduino/src/container/Container.h
@@ -2,11 +2,20 @@
 #define __CONTAINER__
 
 #include "devices/sensors/TempSensor.h"
+#include "devices/light/LightSignals.h"
+#include "devices/light/Led.h"
 #include "ContainerTempDetection.h"
+
+enum System_Status {
+    NORMAL,
+    PROBLEM_DETECTED,
+    };
 
 class Container : public ContainerTempDetection{
     private:
+        System_Status status;
         TempSensor* tempSensor;
+        LightSignals* lightSignals;
     public:
         Container();
         void signalProblem();
