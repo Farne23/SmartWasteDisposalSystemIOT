beb0509a2bc5428c2727ae2adbe44ecf3454ff39
Michele Farneti <michele.farneti@studio.unibo.it>
2024-11-21 15:16:02 +0100

    Added base for door mechanism and fixed timer2 library

--- Arduino/src/devices/actuators/Door.h
+++ Arduino/src/devices/actuators/Door.h
@@ -3,9 +3,10 @@
 
 class Door {
     public:
+        virtual void Door(ServoMotor* Servo)=0;
         virtual void open() = 0;
         virtual void close() = 0;
-        virtual void emptying() = 0;
+        virtual void empty() = 0;
 };
 
 #endif
